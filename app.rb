724bb9eca5efb06fc947ab5be46bea22bd577bf8
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-09-23 18:49:39 +0000

    changes 0923

--- app.rb
+++ app.rb
@@ -9,19 +9,18 @@ get("/") do
   erb(:home)
 end 
 
-get("/user_word") do 
+get("/user_word/") do 
    
   
   @ans = params.fetch("users_words")
- pp @ans 
+  
   raw_ans = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{@ans}")
 
   pro = JSON.parse(raw_ans)
 
  @solution =  pro[0].fetch("meanings")[0].fetch("definitions")[0].fetch("definition")
  @meanings =  pro[0].fetch("meanings")
- 
-
+ @example =   pro[0].fetch("meanings")[0].fetch("definitions")[0].fetch("example")
   erb(:results)
 end   
 
