395d4c632beb643ac5799160d4e38aa53603d337
Brian Waddell <139478254+Brian-Waddell@users.noreply.github.com>
2023-09-23 03:21:15 +0000

    changes

--- app.rb
+++ app.rb
@@ -1,9 +1,39 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
+require "json"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
-end
+
+  
+  erb(:home)
+end 
+
+get("/:user_word") do 
+   
+  
+  @ans = params.fetch("user_word")
+
+  raw_ans = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{@ans}")
+
+  pro = JSON.parse(raw_ans)
+
+ @solution = pro.fetch("definition")
+
+  erb(:second)
+end 
+
+get ("/:user_word/:results") do 
+
+
+ @ans = params.fetch("user_word")
+
+  raw_ans = HTTP.get("https://api.dictionaryapi.dev/api/v2/entries/en/#{@ans}")
+
+  pro = JSON.parse(raw_ans)
+
+ @solution = pro.fetch("definition")
+
+
+ erb(:results)
+end 
